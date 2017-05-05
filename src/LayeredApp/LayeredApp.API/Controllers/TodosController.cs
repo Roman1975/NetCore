204f67fa9bab96add65b96182895792c212bb399
Roman <roman@wst112.tic.com.ua>
2017-05-05 12:02:03 +0300

    fixed usage of todologic

--- src/LayeredApp/LayeredApp.API/Controllers/TodosController.cs
+++ src/LayeredApp/LayeredApp.API/Controllers/TodosController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LayeredApp.Application;
+using LayeredApp.Application.Interface;
 using LayeredApp.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,18 +12,18 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class TodosController : Controller
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly ITodoLogic _todoLogic;
 
         public TodosController(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _todoLogic = new TodoLogic(unitOfWork);
 
         }
         // GET api/values
         [HttpGet]
         public IActionResult Get()
         {
-            var items = _unitOfWork.TodoRepository.GetAll();
+            var items = _todoLogic.Get();
             return Ok(items);
         }
     }
