912d10ff0965485a913d6412ab99dd7072b608f3
Roman <romanchuk.r@gmail.com>
2018-05-16 18:09:17 +0300

    fix netcore 2.0

--- DoubleDbProvider/src/api/Startup.cs
+++ DoubleDbProvider/src/api/Startup.cs
@@ -11,17 +11,6 @@ namespace DoubleDbProvider.API
 {
     public class Startup
     {
-
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName} .json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
